16b0cd1f81fd53372a102d56d215103f2cfc9390
Pavel Malyshev <pavelmalychev@gmail.com>
2017-02-14 16:10:43 +0200

    style(): validator.js
    Styling changes for validator.js
    
    No

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,15 +1,15 @@
 import emailValidator from 'email-validator';
 
 const MIN_PASSWORD_LENGTH = 6;
-const CAN_NOT_BE_BLANK = `Can't be blank`;
+//const CAN_NOT_BE_BLANK = 'Can\'t be blank';
 
 export const validateForgot = ({ email }) => {
     if (!email) {
-        return { email: `Can't be blank` };
+        return { email: 'Can\'t be blank' };
     }
 
     if (!emailValidator.validate(email)) {
-        return { email: `Invalid Email` };
+        return { email: 'Invalid Email' };
     }
 
     return {};
@@ -19,7 +19,7 @@ export const validateLogin = ({ email, password }) => {
     const errors = {};
 
     if (!email) {
-        errors.email = `Can't be blank`;
+        errors.email = 'Can\'t be blank';
     } else if (!emailValidator.validate(email)) {
         errors.email = 'Invalid Email';
     }
@@ -35,25 +35,25 @@ export const validateChangePassword = ({ currentPass, newPass, confirmPass }) =>
     const errors = {};
 
     if (!currentPass) {
-        errors.currentPass = `Can't be blank`;
+        errors.currentPass = 'Can\'t be blank';
     } else if (currentPass.length < MIN_PASSWORD_LENGTH) {
-        errors.currentPass = `Not valid password`;
+        errors.currentPass = 'Not valid password';
     }
 
     if (newPass && confirmPass && newPass !== confirmPass) {
-        errors.confirmPass = `Passwords not matched`;
+        errors.confirmPass = 'Passwords not matched';
     }
 
     if (!newPass) {
-        errors.newPass = `Can't be blank`;
+        errors.newPass = 'Can\'t be blank';
     } else if (newPass.length < MIN_PASSWORD_LENGTH) {
-        errors.newPass = `Not valid password`;
+        errors.newPass = 'Not valid password';
     }
 
     if (!confirmPass) {
-        errors.confirmPass = `Can't be blank`;
+        errors.confirmPass = 'Can\'t be blank';
     } else if (confirmPass.length < MIN_PASSWORD_LENGTH) {
-        errors.confirmPass = `Not valid password`;
+        errors.confirmPass = 'Not valid password';
     }
 
     return errors;
