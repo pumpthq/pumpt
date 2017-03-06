a2334d1800599c06f14e1a286f48db4ddc7a8f66
Pavel Malyshev <pavelmalychev@gmail.com>
2017-03-06 16:30:05 +0200

    fix(): Candidate links
    PUM-327

--- src/containers/application/candidate/steps/addSocial/filled.js
+++ src/containers/application/candidate/steps/addSocial/filled.js
@@ -5,6 +5,8 @@ import LinkedInIcon from './../../../../../components/icons/linkedIn'
 import TwitterIcon from './../../../../../components/icons/twitter'
 import FacebookIcon from './../../../../../components/icons/facebook'
 
+import { formatUrl } from './../../../../../utils';
+
 
 @connect(
     function mapStateToProps(state, ownProps) {
@@ -30,20 +32,24 @@ class AddSocialFilled extends Component {
             facebookProfileUrl
         } = this.props
 
+        let linkedInFormatted = formatUrl(linkedInProfileUrl)
+        let twitterFormatted = formatUrl(twitterUsername, true)
+        let facebookFormatted = formatUrl(facebookProfileUrl)
+
         return (
             <DescriptionListItem>
                 <div class="list__item-general social-block">
                     { linkedInProfileUrl ? <p class="text text_size_s social-block__item">
                         <LinkedInIcon/>
-                        <span class="icon__text">{linkedInProfileUrl}</span>
+                        <span class="icon__text"><a href={linkedInFormatted.url} target="_blank">{linkedInFormatted.displayAs}</a></span>
                     </p> : '' }
                     { twitterUsername ? <p class="text text_size_s social-block__item">
                         <TwitterIcon/>
-                        <span class="icon__text">{`@${twitterUsername}`}</span>
+                        <span class="icon__text"><a href={twitterFormatted.url} target="_blank">{twitterFormatted.displayAs}</a></span>
                     </p> : '' }
                     { facebookProfileUrl ? <p class="text text_size_s social-block__item">
                         <FacebookIcon/>
-                        <span class="icon__text">{facebookProfileUrl}</span>
+                        <span class="icon__text"><a href={facebookFormatted.url} target="_blank">Facebook profile</a></span>
                     </p> : '' }
                 </div>
             </DescriptionListItem>
