4ae3f646947e240486011ffef0f64e321a278297
Chippy <chippy@case.edu>
2017-03-27 16:46:18 -0400

    feat: Small configuration changes + general setup on project
    n/a

--- src/containers/application/candidate/steps/education/container.js
+++ src/containers/application/candidate/steps/education/container.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+//import { reduxReactRouter } from 'redux-router'
 import { connect } from 'react-redux'
 
 import EducationBlockDisplay from './display'
