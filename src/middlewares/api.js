fad821bb002951e5e398936dab18db0446dce251
Jason Yung <json.yung@gmail.com>
2017-04-21 18:46:00 -0400

    Added error handling for apiMiddleware

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -6,8 +6,7 @@ export default apiMiddleware = store => next => action => {
     if(action.type !== API) return next(action)
 
     const {url, success} = action.payload
-    axios.get(API_URL + url).then(res => {
-        store.dispatch(payload.success(res.data))
-    })
-    // TODO: handle error from axios request
+    return axios.get(API_URL + url)
+    .then(res => store.dispatch(payload.success(res.data)))
+    .catch(err => store.dispatch(payload.error(err)))
 }
