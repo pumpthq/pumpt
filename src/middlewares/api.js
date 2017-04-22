8d5a78ad3b8a2a8e5f000535a64562d26180d134
Jason Yung <json.yung@gmail.com>
2017-04-22 02:00:06 -0400

    Patched apiMiddleware

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,12 +1,13 @@
 //API Middleware
 import axios from 'axios'
 import { API_URL } from '../constants/api'
+import { API } from '../constants/actionTypes'
 
-export default apiMiddleware = store => next => action => {
+export default store => next => action => {
     if(action.type !== API) return next(action)
 
-    const {url, success} = action.payload
-    return axios.get(API_URL + url)
-    .then(res => store.dispatch(payload.success(res.data)))
-    .catch(err => store.dispatch(payload.error(err)))
+    const {url, success, error} = action.payload
+    axios.get(API_URL + url)
+    .then(res => store.dispatch(success(res.data)))
+    .catch(err => store.dispatch(error(err)))
 }
