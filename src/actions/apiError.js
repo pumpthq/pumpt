52272ef6c6771737db77e8d255e6fb02d48345d8
Jason Yung <json.yung@gmail.com>
2017-05-06 17:58:38 -0400

    Updated api middleware
    dispatching action when api endpoint is requested

--- src/actions/apiError.js
+++ src/actions/apiError.js
@@ -1,6 +1,7 @@
 export const RESET_API_ERROR = 'RESET_API_ERROR';
 export const ADD_API_ERROR = 'ADD_API_ERROR';
 export const API_ERROR = 'API_ERROR'
+export const API_REQUEST = 'API_REQUEST'
 export const API_SUCCESS = 'API_SUCCESS'
 
 export const addApiError = message => ({
@@ -20,4 +21,9 @@ export const fetchFailed = err => ({
 export const fetchSucceeded = data => ({
     type : API_SUCCESS,
     payload : { data }
+})
+
+export const fetchRequested = props => ({
+    type : API_REQUEST,
+    ...props
 })
--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -2,14 +2,20 @@
 import axios from 'axios'
 import { API_URL } from '../constants/api'
 import { API } from '../constants/actionTypes'
-import { fetchSucceeded, fetchFailed } from '../actions/apiError'
+import { fetchRequested, fetchSucceeded, fetchFailed } from '../actions/apiError'
 
 export default store => next => action => {
-    if(action.type !== API) return next(action)
 
-    const {url, data, success = fetchSucceeded, error = fetchFailed, method = 'get'} = action.payload
+    next(action) //always allow action to continue through middleware
 
-    axios({ baseURL: API_URL, method, url, data })
-    .then(res => store.dispatch(success(res.data)))
-    .catch(err => store.dispatch(error(err)))
+    const { type, payload } = action
+    if(type === API) {
+      const {url, data, request = fetchRequested, success = fetchSucceeded, error = fetchFailed, method = 'get'} = payload
+
+      store.dispatch(request({payload}))
+
+      axios({ baseURL: API_URL, method, url, data })
+      .then(res => store.dispatch(success(res.data)))
+      .catch(err => store.dispatch(error(err)))
+    }
 }
