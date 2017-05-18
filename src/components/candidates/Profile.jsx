6979b75e6f0699e264ffa77910e7293cf55cbbc1
Jason Yung <json.yung@gmail.com>
2017-05-17 21:29:22 -0400

    Prototype CandidateProfile view

--- src/components/candidates/Profile.jsx
+++ src/components/candidates/Profile.jsx
@@ -15,25 +15,46 @@ export default class CandidateProfile extends Component {
         const { firstName, lastName, avatar } = candidate
         const { title } = job
 
-        console.log(candidate)
         return (
 
             <div className="mdl-card card card_type_mini card_state_open">
                 <a class="button_type_close" onClick={browserHistory.goBack}>×</a>
 
-                <h2>{ title }</h2>
+                <h2>Job: { title }</h2>
 
                 <CardDivider />
 
-                <img src={avatar}></img>
+                <div className="col-xs-4">
+                    <img src={avatar}></img>
+                </div>
 
-                { `${lastName}, ${firstName}` }
+                <div className="col-xs-8">
+                    <h4>{ `${lastName}, ${firstName}` }</h4>
+                    <LabeledValue label="Recent Income" value={candidate.recentAnnualIncome} />
+                    <LabeledValue label="Recent Job" value={candidate.recentJob} />
+                    <LabeledValue label="Experience Duration" value={candidate.recentAreaExperience} />
+
+                </div>
+                <CardDivider />
+
+                <div className="col-xs-12">
+                    <pre>
+                        { JSON.stringify(candidate, null, 4) }
+                    </pre>
+                </div>
+
 
             </div>
         )
     }
 }
 
+const LabeledValue = (props) => (
+    <div className={props.className || 'labeled-value'} >
+        <label>{props.label}:</label>
+        <span>{props.value}</span>
+    </div>
+)
 const CardDivider = () => (<div className="summary-head__title-item summary-head__title-item_type_alignment summary-head__title-item_type_middle"></div>)
 
 CandidateProfile.propTypes = propTypes;
