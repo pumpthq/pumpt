3125a70b5ebead4e0847a973dd88b8e3205346ae
Koby Picker <jkp46@case.edu>
2018-01-23 18:38:59 -0800

    text wrap, labels, and material ui for candidate application
    Signed-off-by: Koby Picker <jkp46@case.edu>

--- src/components/candidates/Form.jsx
+++ src/components/candidates/Form.jsx
@@ -3,7 +3,7 @@ import { reduxForm, Field, SubmissionError, formValueSelector } from 'redux-form
 import { connect } from 'react-redux'
 import { Location, EnumSelector, TextArea, TextInput } from 'components/form/inputs'
 import MultiInput from 'components/main/form/MultiInput'
-import { TextField } from 'material-ui'
+import { TextField, SelectField, MenuItem } from 'material-ui'
 import './form.less'
 
 //Actions
@@ -22,7 +22,10 @@ import {
 
 
 export const renderTextField = ({
-  input,
+  input: {
+    value,
+    onChange
+  },
   label,
 	className,
   meta: { asyncValidating, touched, error }
@@ -30,8 +33,8 @@ export const renderTextField = ({
   <div class={className}>
 		<div class={asyncValidating ? 'async-validating' : 'class'}>
       <TextField
-        class="mdl-textfield__input textfield__input"
-        {...input}
+        value={value}
+        onChange={onChange}
         placeholder={label}
         floatingLabelText={label} />
       {touched && error && <span class="textfield__error">{error}</span>}
@@ -39,12 +42,14 @@ export const renderTextField = ({
   </div>
 )
 
-const renderSelectField = ({ input, label, type, meta: { touched, error }, children }) => (
+const renderSelectField = ({ input: {value, onChange}, label, meta: { touched, error }, children }) => (
     <div>
-      <select {...input} class="mdl-textfield__input textfield__input textfield__light">
-				<option value="" class="disabled-text-option" disabled selected>{label}</option>
+      <SelectField
+        value={value}
+        onChange={(event, id, payload) => (onChange(payload))}
+        floatingLabelText={label}>
         {children}
-      </select>
+      </SelectField>
       {touched && error && <span>{error}</span>}
     </div>
 )
@@ -76,68 +81,66 @@ let CandidateSummaryForm = props =>  {
 
 			<form onSubmit={handleSubmit} class="candidate-edit-form"> 
 				<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-3 col-md-offset-3">
 						<Field
 							name="firstName"
 							component={renderTextField}
 							label="First Name"
 						/>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-3">
 						<Field
 							name="lastName"
 							component={renderTextField}
 							label="Last Name"
 						/>
 					</div>
+        </div>
+        <div class="row">
 					<div class="col-md-12">
 						<div class="row">
-							{industryValue ?
-								<div class="col-md-4">
-									<Field name="recentAnnualIncome" component={renderSelectField} label="Income" class="mdl-textfield__input textfield__input textfield__light">
-										{ ANNUAL_INCOME_DROPDOWN_DATA.map(item => <option key={item.id}  value={item.title}>{item.title}</option>) }
-									</Field>
-								</div>
-								:
-								<div class="col-md-4 col-md-offset-4">
-									<Field name="recentAnnualIncome" component={renderSelectField} label="Income" class="mdl-textfield__input textfield__input textfield__light">
-										{ ANNUAL_INCOME_DROPDOWN_DATA.map(item => <option key={item.id}  value={item.title}>{item.title}</option>) }
-									</Field>
-								</div>
-							}
 							<div class="col-md-4">
-								<Field name="recentJob" component={renderSelectField} label="Job Title" class="mdl-textfield__input textfield__input textfield__light">
-									{ JOB_TITLE_DROPDOWN_DATA[0].items.map(item => <option key={item.id}  value={item.title}>{item.title}</option>) }
+                <Field
+                  name="recentAnnualIncome"
+                  component={renderSelectField} label="Income">
+                  { ANNUAL_INCOME_DROPDOWN_DATA.map(item => <MenuItem key={item.id}  value={item.title} primaryText={item.title}/>) }
 								</Field>
 							</div>
 							<div class="col-md-4">
-								<Field name="recentAreaExperience" component={renderSelectField} label="Industry Experience" class="mdl-textfield__input textfield__input textfield__light">
-									{ EXPERIENCE_DROPDOWN_DATA.map((item) => {return <option key={item.id}  value={item.title}>{item.title}</option>}) }
+                <Field
+                  name="recentAreaExperience"
+                  component={renderSelectField} label="Industry Experience">
+									{ EXPERIENCE_DROPDOWN_DATA.map((item) => (<MenuItem key={item.id}  value={item.title} primaryText={item.title}/>)) }
 								</Field>
 							</div>
+							<div class="col-md-4">
+                <Field
+                  name="recentJob"
+                  component={renderSelectField} label="Job Title">
+									{ JOB_TITLE_DROPDOWN_DATA[0].items.map(item => <MenuItem key={item.id}  value={item.title} primaryText={item.title}/>) }
+								</Field>
+							</div>
+            </div>
+            <div class="row">
 							<div class="col-md-4">
                 <label>Industry</label>
                 <Field 
                   name="interestWorkingArea"
                   component={MultiInput}
                   label="Industry"
-                  values={INDUSTRY_DROPDOWN_DATA[0].items.map((item) => (item.title))}
-                  class="mdl-textfield__input textfield__input textfield__light">
+                  values={INDUSTRY_DROPDOWN_DATA[0].items.map((item) => (item.title))}>
 								</Field>
 							</div>
 							<div class="col-md-4">
+                <label>Expertise</label>
                 <Field 
                   name="recentWorkingAreaParent"
                   component={renderSelectField}
-                  label="Recent Working Area"
-                  class="mdl-textfield__input textfield__input textfield__light">
-									{ FIELD_OF_EXPERTISE_DROPDOWN_DATA.map((item) => {return <option key={item.id} value={item.title}>{item.title}</option>}) }
+                  label="Field Of Expertise">
+									{ FIELD_OF_EXPERTISE_DROPDOWN_DATA.map((item) => <MenuItem key={item.id} value={item.title} primaryText={item.title}/>)}
 								</Field>
-							</div>
-							<div class="col-md-4">
 								{industryValue && 
                   <div>
-                    <label>Specialty</label>
                     <Field
                       name="recentWorkingArea"
                       component={MultiInput}
--- src/components/candidates/Summary.jsx
+++ src/components/candidates/Summary.jsx
@@ -43,21 +43,29 @@ const CandidateSummary = ({
       </div>
       <div className="summary-head__title-item summary-head__title-item_type_alignment">
         <div className="summary-head__title-column">
-          <span className="text summary-head__label">Industry</span>
-          <span className="text text_size_s summary-head__summary">
-            {interestWorkingArea && Array.isArray(interestWorkingArea) ?
-                interestWorkingArea.join(', ') : interestWorkingArea}
-          </span>
           <span className="text summary-head__label">Annual Income</span>
           <span className="text text_size_s summary-head__summary">{recentAnnualIncome}</span>
+        </div>
+        <div className="summary-head__title-column">
+          <span class="text summary-head__label">Industry Experience </span>
+          <span class="text text_size_s summary-head__summary">{recentAreaExperience}</span>
+        </div>
+        <div className="summary-head__title-column">
+          <span className="text summary-head__label">Job Title </span>
+          <span className="text text_size_s summary-head__summary">{recentJob}</span>
         </div>
         <div className="summary-head__title-column">
           <span className="text summary-head__label">Working Areas</span>
           <span className="text text_size_s summary-head__summary">
             {displayIndustries(recentWorkingAreas)}
           </span>
-          <span className="text summary-head__label">Job Title </span>
-          <span className="text text_size_s summary-head__summary">{recentJob}</span>
+        </div>
+        <div className="summary-head__title-column">
+          <span className="text summary-head__label">Industry</span>
+          <span className="text text_size_s summary-head__summary">
+            {interestWorkingArea && Array.isArray(interestWorkingArea) ?
+                interestWorkingArea.join(', ') : interestWorkingArea}
+          </span>
         </div>
       </div>
     </div>
