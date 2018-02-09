c14cfc4625afbcfd721994eb94e10dfdd4419113
Koby Picker <jkp46@case.edu>
2018-02-09 10:57:46 -0800

    update year, dont pass null values to api
    Signed-off-by: Koby Picker <jkp46@case.edu>

--- src/components/main/form/normalizations.jsx
+++ src/components/main/form/normalizations.jsx
@@ -8,7 +8,7 @@ export const normalizeDate = value => {
 		const minMonth = onlyNums.slice(0,2).replace(/^00/,'01')
 		const month = minMonth.replace(/^[2-9][0-9]|1[3-9]$/,'12')
 		const minYear = onlyNums.slice(2).replace(/^[0-1][0-8][0-9][0-9]$/,'1900')
-		const year = minYear.replace(/^[2-9][1-9][0-9][0-9]|20[2-9][0-9][0-9]|201[8-9]$/,'2017')
+		const year = minYear.replace(/^[2-9][1-9][0-9][0-9]|20[2-9][0-9][0-9]|2019$/,'2018')
 
 		return `${month}/${year.slice(0,4)}`
 	}
