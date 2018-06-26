97b1f1ae374ed548545b14263a419b055edc3711
Koby Picker <jkp46@case.edu>
2018-06-26 08:07:11 -0400

    Revert "Revert "Candidate Header Menu reflects location via react-router + activeClass""
    This reverts commit 456ed485703a52151fcaa90f0138c842e4ff4c39.

--- src/history.js
+++ src/history.js
@@ -1,9 +1,9 @@
-import {hashHistory} from 'react-router'
+import {browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 import store from './store';
 
-const history = syncHistoryWithStore(hashHistory, store, {
-    adjustUrlOnReplay : false // the URL will not be kept in sync during time travel
+const history = syncHistoryWithStore(browserHistory, store, {
+    adjustUrlOnReplay : false, // the URL will not be kept in sync during time travel
 })
 
 export default history
